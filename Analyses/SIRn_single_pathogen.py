026f7cf7e444b289f12750e1ba7c454913a8a7e9
BenSinger01 <benjaminjohnsinger@gmail.com>
2024-09-19 11:49:11 -0700

    Rota model before meeting with Joe on Sep 19

--- Analyses/SIRn_single_pathogen.py
+++ Analyses/SIRn_single_pathogen.py
@@ -4,6 +4,8 @@
 import numpy as np
 import scipy as sp
 import itertools as it
+import contact_model as cm
+
 ## Period of simulation in months
 PERIOD = 12*35
 
@@ -27,15 +29,16 @@ CONTACT = np.genfromtxt('Data/Processed/contact_matrices/KP_contact_all_US_Censu
 ## Parameters - rotavirus in this example
 # Number of susceptibility classes
 N_S = 3
-# Immunity waning rates, including maternal immunity
-WANE = np.array([1/3,1/9,1/9,1/12])
-# Recovery rates
+# Immunity waning rates for each susceptibility class, including maternal immunity
+WANE_UP = np.array([1/3,1/9,1/9,0]) # Waning to higher susceptibility class
+WANE_SAME = np.array([0,0,0,1/12]) # Waning to same susceptibility class
+# Recovery rates for each susceptibility class
 REC = np.array([4.3,8.6,8.6])
-# Relative susceptability and immunity, for each infection compartment
+# Relative susceptability and immunity, for each susceptibility class
 S_REL = np.array([1,0.62,0.35])
 I_REL = np.array([[1],[0.5],[0.1]])
 I_REL_3D = np.array([[[1]],[[0.5]],[[0.1]]])
-# Probability of detection of cases
+# Probability of detection of cases for each susceptibility class
 P_OBS = 0.041*np.array([0.11,0.029,0])
 
 ## Time-varying parameters
@@ -43,14 +46,13 @@ P_OBS = 0.041*np.array([0.11,0.029,0])
 S_VAX = 2 # Which susceptibility class is vaccination equivalent to?
 COVERAGE = 0.96*0.8
 T_VAX = 300
-def VAX(t):
-    return COVERAGE if t >= T_VAX else 0
+def VAX(t,S_CLASS):
+    return COVERAGE if (t >= T_VAX) & (S_CLASS == S_VAX) else 0
 # Force of infection per contact
 SEASONALITY = 0.055
 OFFSET = 0.636
-def BETA_PC(t):
-    return 23.25*4.3/12.99 # Pitzer beta divided by average number of contacts
-def FOI_PC(t):
+BETA_PC = cm.BETA_PC_STATIC
+def INFECTIOUS_CONTACT(t):
     return BETA_PC(t)*(1+SEASONALITY*np.cos(2*np.pi*(t/12-OFFSET)))
 
 ## Initial conditions
@@ -59,64 +61,76 @@ STATE0[0] = KP_AGE_POP[0] # Infants in maternal immunity compartment
 STATE0[NAG+1:2*NAG] = KP_AGE_POP[1:]-1 # Every other age group is all susceptible except
 STATE0[2*NAG+1:3*NAG] = 1 # one individual in each age group that is infected.
 
-
 ## Differential equations
-def deltas(state,t):
+def deltas(t,state,params):
     delta = np.zeros(state.shape)
     pop_size = np.sum(state)
+    NAG, N_S, AGING_RATE, BIRTH_RATE, WANE_UP, WANE_SAME, REC, S_REL, I_REL, P_OBS, S_VAX, VAX, INFECTIOUS_CONTACT = params
     # Maternal immunity - birth into this compartment, waning, aging
-    delta[0:NAG] = (1-VAX(t))*BIRTH_RATE*pop_size - WANE[0]*state[0:NAG]\
+    delta[0:NAG] = (1-VAX(t,S_VAX))*BIRTH_RATE*pop_size - WANE_UP[0]*state[0:NAG]\
         - AGING_RATE*state[0:NAG] + np.concatenate((np.zeros(1), AGING_RATE[:-1]*state[0:NAG-1]))
     # Susceptible, infected, recovered - waning, aging, infection, recovery for all susceptibility classes
     for i in range(N_S):
-        delta[(3*i+1)*NAG:(3*i+2)*NAG] = -S_REL[i]*FOI_PC(t)*(np.dot(CONTACT,np.sum(np.array(([state[(3*j+2)*NAG:(3*j+3)*NAG] for j in range(N_S)]))*I_REL,axis=0))/pop_size)*state[(3*i+1)*NAG:(3*i+2)*NAG] + WANE[i]*state[(3*i)*NAG:(3*i+1)*NAG]\
+        delta[(3*i+1)*NAG:(3*i+2)*NAG] = VAX(t,i)*BIRTH_RATE*pop_size\
+            -S_REL[i]*(np.dot(INFECTIOUS_CONTACT(t),np.sum(np.array(([state[(3*j+2)*NAG:(3*j+3)*NAG] for j in range(N_S)]))*I_REL,axis=0))/pop_size)*state[(3*i+1)*NAG:(3*i+2)*NAG]\
+            + WANE_UP[i]*state[(3*i)*NAG:(3*i+1)*NAG] + WANE_SAME[i+1]*state[(3*i+3)*NAG:(3*i+4)*NAG]\
             - AGING_RATE*state[(3*i+1)*NAG:(3*i+2)*NAG] + np.concatenate((np.zeros(1), AGING_RATE[:-1]*state[(3*i+1)*NAG:(3*i+2)*NAG-1]))
-        if i == S_VAX:
-            delta[(3*i+1)*NAG:(3*i+2)*NAG] += VAX(t)*BIRTH_RATE*pop_size
-        if i == N_S-1:
-            delta[(3*i+1)*NAG:(3*i+2)*NAG] += WANE[-1]*state[(3*i+3)*NAG:(3*i+4)*NAG]
-        delta[(3*i+2)*NAG:(3*i+3)*NAG] = S_REL[i]*FOI_PC(t)*(np.dot(CONTACT,np.sum(np.array(([state[(3*j+2)*NAG:(3*j+3)*NAG] for j in range(N_S)]))*I_REL,axis=0))/pop_size)*state[(3*i+1)*NAG:(3*i+2)*NAG] - REC[i]*state[(3*i+2)*NAG:(3*i+3)*NAG]\
+        delta[(3*i+2)*NAG:(3*i+3)*NAG] = S_REL[i]*(np.dot(INFECTIOUS_CONTACT(t),np.sum(np.array(([state[(3*j+2)*NAG:(3*j+3)*NAG] for j in range(N_S)]))*I_REL,axis=0))/pop_size)*state[(3*i+1)*NAG:(3*i+2)*NAG]\
+            - REC[i]*state[(3*i+2)*NAG:(3*i+3)*NAG]\
             - AGING_RATE*state[(3*i+2)*NAG:(3*i+3)*NAG] + np.concatenate((np.zeros(1), AGING_RATE[:-1]*state[(3*i+2)*NAG:(3*i+3)*NAG-1]))
-        delta[(3*i+3)*NAG:(3*i+4)*NAG] = REC[i]*state[(3*i+2)*NAG:(3*i+3)*NAG] - WANE[i+1]*state[(3*i+3)*NAG:(3*i+4)*NAG]\
+        delta[(3*i+3)*NAG:(3*i+4)*NAG] = REC[i]*state[(3*i+2)*NAG:(3*i+3)*NAG]\
+            - (WANE_UP[i+1]+WANE_SAME[i+1])*state[(3*i+3)*NAG:(3*i+4)*NAG]\
             - AGING_RATE*state[(3*i+3)*NAG:(3*i+4)*NAG] + np.concatenate((np.zeros(1), AGING_RATE[:-1]*state[(3*i+3)*NAG:(3*i+4)*NAG-1]))
-    # Enforce stable population size
-    if np.sum(delta) > 0:
-        delta -= np.sum(delta)*state/np.sum(state)
+    # # Enforce stable population size
+    # if np.sum(delta) > 0:
+    #     delta -= np.sum(delta)*state/np.sum(state)
     return delta
 
 ## Integrate the system
-result = sp.integrate.odeint(deltas, STATE0, np.arange(0,PERIOD))
+POINTS = PERIOD
+# result = sp.integrate.odeint(deltas, STATE0, np.linspace(0,PERIOD,POINTS),args=((NAG, N_S, AGING_RATE, BIRTH_RATE, WANE_UP, WANE_SAME, REC, S_REL, I_REL, P_OBS, S_VAX, VAX, INFECTIOUS_CONTACT),))
+result = sp.integrate.solve_ivp(deltas, [0,PERIOD], STATE0, args=((NAG, N_S, AGING_RATE, BIRTH_RATE, WANE_UP, WANE_SAME, REC, S_REL, I_REL, P_OBS, S_VAX, VAX, INFECTIOUS_CONTACT),))
+
+print(result.y.shape)
 
 ## Calculate observations
-foi_pc = np.array([FOI_PC(t) for t in range(PERIOD)])
-pop_size = np.sum(result,axis=1)
-foi = foi_pc*(np.dot(CONTACT,np.sum((np.array([result[:,(3*j+2)*NAG:(3*j+3)*NAG] for j in range(N_S)])*I_REL_3D),axis=0).T)/pop_size)
-obs = np.zeros(PERIOD)
-for i in range(N_S):
-    obs += P_OBS[i]*S_REL[i]*np.sum(foi.T*result[:,(3*i+1)*NAG:(3*i+2)*NAG],axis=1)
+obs = np.zeros(len(result.t))
+pop_size = np.sum(result.y,axis=1)
+for t in result.t:
+    foi = np.dot(INFECTIOUS_CONTACT(t),np.sum((np.array([result.y[(3*j+2)*NAG:(3*j+3)*NAG,t] for j in range(N_S)])*I_REL_3D),axis=0))/pop_size[t]
+    for i in range(N_S):
+        obs[t] += P_OBS[i]*S_REL[i]*np.sum(foi[:,i]*result.y[t,(3*i+1)*NAG:(3*i+2)*NAG])
 
 ## Plot the results
 import matplotlib.pyplot as plt
-plt.plot(obs, label='Observed cases')
-# plt.xlim(0,250)
-plt.xlim(200,PERIOD)
-plt.ylim(0,3e3)
-# plt.plot(np.sum(result[:,0:NAG],axis=1), label='Maternal immunity')
-# plt.plot(np.sum(result[:,1*NAG:2*NAG],axis=1), label='First susceptible class')
-# plt.plot(np.sum(result[:,2*NAG:3*NAG],axis=1), label='First infections')
-# plt.plot(np.sum(result[:,5*NAG:6*NAG],axis=1), label='Second infections')
-# plt.plot(np.sum(result[:,8*NAG:9*NAG],axis=1), label='Asymptomatic infections')
-# plt.xlim(100,PERIOD)
-# plt.ylim(0,2e5)
-# plt.plot(np.sum(result[:,1*NAG:2*NAG],axis=1), label='First susceptible')
-# plt.plot(np.sum(result[:,4*NAG:5*NAG],axis=1), label='Second susceptible')
-# plt.plot(np.sum(result[:,7*NAG:8*NAG],axis=1), label='Asymptomatic susceptible')
-# plt.plot(np.sum(result[:,range(0,10*NAG,NAG)],axis=1), label='Newborns')
-# plt.plot(np.sum(result[:,range(1,10*NAG,NAG)],axis=1), label='Infants')
-# plt.plot(np.sum(result[:,range(2,10*NAG,NAG)],axis=1), label='Young children')
-# plt.plot(np.sum(result[:,range(3,10*NAG,NAG)],axis=1), label='Older children')
-# plt.plot(np.sum(result[:,range(4,10*NAG,NAG)],axis=1), label='Young adults')
-# plt.plot(np.sum(result[:,range(5,10*NAG,NAG)],axis=1), label='Middle-aged adults')
-# plt.plot(np.sum(result[:,range(6,10*NAG,NAG)],axis=1), label='Older adults')
-plt.legend()
+
+viridis = plt.cm.get_cmap('viridis', 7)
+
+plt.plot(result.t,obs/pop_size, label='Observed cases per capita')
+# plt.xlim(19*POINTS/20,POINTS)
+plt.ylim(0,2e-5)
 plt.show()
+
+# fig, axes = plt.subplots(2,2,figsize=(6.5,6.5))
+# axes[0,0].plot(obs/pop_size, label='Observed cases')
+# axes[0,0].set_title('Simulation (including burn-in)')
+# axes[0,0].set_ylabel('Observed incidence')
+# axes[0,1].plot(obs/pop_size, label='Observed cases')
+# axes[0,1].set_xlim(POINTS/2,POINTS)
+# axes[0,1].set_ylim(0,8e-5)
+# axes[0,1].set_title('Detail after burn-in')
+# axes[0,1].set_ylabel('Observed incidence')
+# axes[1,0].plot(pop_size, label='Population size')
+# axes[1,0].set_title('Total population')
+# axes[1,0].set_ylabel('Persons')
+# axes[1,1].plot(np.sum(result[:,range(0,10*NAG,NAG)],axis=1)/pop_size, label='Newborns', color=viridis(0))
+# axes[1,1].plot(np.sum(result[:,range(1,10*NAG,NAG)],axis=1)/pop_size, label='Infants', color=viridis(1))
+# axes[1,1].plot(np.sum(result[:,range(2,10*NAG,NAG)],axis=1)/pop_size, label='Young children', color=viridis(2))
+# axes[1,1].plot(np.sum(result[:,range(3,10*NAG,NAG)],axis=1)/pop_size, label='Older children', color=viridis(3))
+# axes[1,1].plot(np.sum(result[:,range(4,10*NAG,NAG)],axis=1)/pop_size, label='Young adults', color=viridis(4))
+# axes[1,1].plot(np.sum(result[:,range(5,10*NAG,NAG)],axis=1)/pop_size, label='Middle-aged adults', color=viridis(5))
+# axes[1,1].plot(np.sum(result[:,range(6,10*NAG,NAG)],axis=1)/pop_size, label='Older adults', color=viridis(6))
+# axes[1,1].set_title('Age groups')
+# axes[1,1].set_ylabel('Proportion of total population')
+# plt.tight_layout()
+# plt.savefig('Figures/SIR3_rota_demo_pc.png')
