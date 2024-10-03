a3e34270f1b54e7872decfb2ec72988c285c8e9d
BenSinger01 <benjaminjohnsinger@gmail.com>
2024-10-03 15:32:38 -0700

    first pass at SISn model (untested)

---
+++ Analyses/SISn_ODEs.py
@@ -0,0 +1,24 @@
+## Ordinary differential equations defining SIR models with n susceptibility classes
+## BJS September 2024
+
+import numpy as np
+
+## Differential equations
+def single_pathogen_deltas(t,state,params):
+    delta = np.zeros(state.shape)
+    pop_size = np.sum(state)
+    NAG, N_S, AGING_RATE, BIRTH_RATE, WANE, REC, S_REL, S_AGE, I_REL, P_OBS, birth_vax, all_vax, S_VAX, ACOV, BCOV, T_VAX, IMPORT, BETA, contact = params
+    # Susceptible, infected - waning, aging, infection, recovery for all susceptibility classes
+    for i in range(N_S):
+        # Susceptibile class i = birth - infection + waning + aging in - aging out +/- vaccination
+        delta[(2*i)*NAG:(2*i+1)*NAG] = birth_vax(t,i,S_VAX,BCOV,T_VAX)*BIRTH_RATE*pop_size*np.concatenate((np.ones(1),np.zeros(NAG-1)))\
+            -S_REL[i]*S_AGE*BETA*(np.dot(contact(t),np.sum(np.array(([state[(2*j+1)*NAG:(2*j+2)*NAG] for j in range(N_S)]))*I_REL,axis=0))/pop_size)*state[(2*i)*NAG:(2*i+1)*NAG]\
+            - WANE[i]*state[(2*i)*NAG:(2*i+1)*NAG] + WANE[i+1]*state[(2*i+2)*NAG:(2*i+3)*NAG]\
+            - AGING_RATE*state[(2*i)*NAG:(2*i+1)*NAG] + np.concatenate((np.zeros(1), AGING_RATE[:-1]*state[(2*i)*NAG:(2*i+1)*NAG-1]))\
+            + (all_vax(t,i,ACOV,S_VAX,T_VAX,NAG,N_S)*state).reshape((2*N_S+1,NAG)).sum(axis=0)
+        # Infectious class i = infection - recovery + aging in - aging out - vaccination + importations
+        delta[(2*i+1)*NAG:(2*i+2)*NAG] = S_REL[i]*S_AGE*BETA*(np.dot(contact(t),np.sum(np.array(([state[(2*j+1)*NAG:(2*j+2)*NAG] for j in range(N_S)]))*I_REL,axis=0))/pop_size)*state[(2*i)*NAG:(2*i+1)*NAG]\
+            - REC[i]*state[(2*i+1)*NAG:(2*i+2)*NAG]\
+            - AGING_RATE*state[(2*i+1)*NAG:(2*i+2)*NAG] + np.concatenate((np.zeros(1), AGING_RATE[:-1]*state[(2*i+1)*NAG:(2*i+2)*NAG-1]))\
+            + IMPORT
+    return delta
