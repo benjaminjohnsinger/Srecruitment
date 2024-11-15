231420a1a70851793fb5a318bdb244442cd3bba2
BenSinger01 <benjaminjohnsinger@gmail.com>
2024-11-15 14:53:40 -0800

    processed kpsc clinical case data into time series

---
+++ Analyses/Parameters/census_population.py
@@ -0,0 +1,40 @@
+## Test population parameters
+## Rough estimates of population size and age distribution
+
+import numpy as np
+
+## Population size and age distribution
+
+# SC_COUNTY_POPS = {"Los Angeles":10081570,"Kern":887641,"Orange":3168044,"San Diego":3316073,"San Bernardino":2149031,"Riverside":2411439,"Ventura":847263}
+
+print(21042.0+35138.0+133997.0+734577.0+1431222.0+1447198.0+786872.0)
+print(19761.0+33526.0+129692.0+716100.0+1227570.0+1297508.0+600336.0)
+
+POP_SIZE = 4024493 # KPSC insured population size in 2015
+
+AGE_POP = np.genfromtxt('Data/Processed/US_Census_population_by_age.csv', delimiter=',') # US in 2022
+AGE_GROUPS = [
+    range(3),
+    range(3,12),
+    range(12,5*12),
+    range(5*12,18*12),
+    range(18*12,40*12),
+    range(40*12,65*12),
+    range(65*12,100*12)]
+NAG = len(AGE_GROUPS)
+MEDIAN_AGE = [np.median(AGE_GROUPS[i]) for i in range(len(AGE_GROUPS))]
+AGE_GROUP_NAMES = ['Newborns','Infants','Young children','Older children','Young adults','Middle-aged adults','Older adults']
+
+# Age group proportions for California in 2022
+AGE_PROPORTION = np.array([np.sum(AGE_POP[group]) for group in AGE_GROUPS])/np.sum(AGE_POP)
+CENSUS_AGE_POP = AGE_PROPORTION*POP_SIZE
+
+# Rate of aging out of each age group. Last rate informed by US life expectancy at age 65.
+# AGING_RATE = 1/np.array([3/12*365,9/12*365,4*365,13*365,22*365,25*365,18.35*365])
+# Same with last rate informed by US life expectancy at birth. This leads to a more stable age distribution and population size.
+AGING_RATE = 1/np.array([3/12*365,9/12*365,4*365,13*365,22*365,25*365,12.43*365])
+
+
+# Birth rate (for California in 2022)
+BIRTH_RATE = 3.99e5/(POP_SIZE*365)
+# AGING_RATE = BIRTH_RATE/AGE_PROPORTION # Stable population distribution
--- Analyses/data_processing.py
+++ Analyses/data_processing.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sas7bdat import SAS7BDAT
+from matplotlib.cm import hsv
 
 # with SAS7BDAT('Data/Raw/KPSC/clinical.sas7bdat') as f:
 #     clinical_data = f.to_data_frame()
@@ -20,10 +21,31 @@ from sas7bdat import SAS7BDAT
 
 # load hospitalizations
 hospitalizations = pd.read_csv('Data/Processed/KPSC_clinical_hospitalizations.csv')
+# format date
 hospitalizations["Date"] = pd.to_datetime(hospitalizations["Date"])
-
 hospitalizations["Month"] = hospitalizations["Date"].dt.month
 hospitalizations["Year"] = hospitalizations["Date"].dt.year
+
+# load age population data
+age_by_year = pd.read_csv("Data/Processed/KPSC_population_by_age.csv")
+age_by_year["Year"] = np.arange(2015,2023)
+age_by_year = age_by_year.set_index("Year")
+age_by_year.columns = ['<3mo','3-12mo','1-4y','5-17y','18-39y','40-64y','>=65y']
+# sum '<3mo','3-12mo' into <1y
+age_by_year['<1y'] = age_by_year['<3mo'] + age_by_year['3-12mo']
+age_by_year = age_by_year.drop(columns=['<3mo','3-12mo'])
+# order columns
+age_by_year = age_by_year[['<1y','1-4y','5-17y','18-39y','40-64y','>=65y']]
+# repeat last row for 2023
+age_by_year.loc[2023] = age_by_year.loc[2022]
+
+
+# assign each row to age group
+AGE_GROUPS = [range(0),range(1),range(1,5),range(5,18),range(18,40),range(40,65),range(65,100)]
+AGE_GROUP_NAMES = ['0','<1y','1-4y','5-17y','18-39y','40-64y','>=65y']
+for i in range(len(AGE_GROUPS)):
+    hospitalizations.loc[hospitalizations["age"].isin(AGE_GROUPS[i]),"age group"] = AGE_GROUP_NAMES[i]
+
 
 # get only influenza hospitalizations, i.e. CODE starts with J09, J10, or J11
 flu_hospitalizations = hospitalizations[hospitalizations["CODE"].str.contains('J09|J10|J11')]
@@ -32,55 +54,78 @@ rsv_hospitalizations = hospitalizations[hospitalizations["CODE"].str.contains('J
 # get only COVID-19 hospitalizations, i.e. CODE is U07.1
 covid_hospitalizations = hospitalizations[hospitalizations["CODE"].str.contains('U07.1')]
 
-# # filter out just one row for each value of StudyID
-flu_hospitalizations = flu_hospitalizations.drop_duplicates(subset="StudyID")
-rsv_hospitalizations = rsv_hospitalizations.drop_duplicates(subset="StudyID")
-covid_hospitalizations = covid_hospitalizations.drop_duplicates(subset="StudyID")
+def month_format(hosps,age_strat=True,incidence=False):
+    # # filter out just one row for each value of StudyID
+    hosps = hosps.drop_duplicates(subset="StudyID")
+    if age_strat:
+        hosps = hosps.groupby(["Year","Month","age group"]).size().reset_index(name='Count')
+    else:
+        hosps = hosps.groupby(["Year","Month"]).size().reset_index(name='Count')
+    # # fill in zeros for missing months and age groups
+    for date in pd.date_range(start='2015-10-01',end='2023-09-30',freq='MS'):
+        year = date.year
+        month = date.month
+        if age_strat:
+            for age_group in AGE_GROUP_NAMES:
+                if not (((hosps["Year"] == year) & (hosps["Month"] == month)) & (hosps["age group"] == age_group)).any():
+                    hosps = pd.concat([hosps,pd.DataFrame({"Year":[year],"Month":[month],"age group":[age_group],"Count":[0]})],ignore_index=True)
+        else:
+            if not ((hosps["Year"] == year) & (hosps["Month"] == month)).any():
+                hosps = pd.concat([hosps,pd.DataFrame({"Year":[year],"Month":[month],"Count":[0]})],ignore_index=True)
+    hosps["Date"] = pd.to_datetime(hosps["Year"].astype(str) + '-' + hosps["Month"].astype(str) + '-01')
+    # sort by date
+    hosps = hosps.sort_values(by="Date")
+    # date as index, remove year month and date as columns
+    hosps = hosps.set_index("Date")
+    hosps = hosps.drop(columns=["Year","Month"])
+    if age_strat:
+        # columns as age groups
+        hosps = hosps.pivot(columns="age group",values="Count")
+        # drop 0 column
+        hosps = hosps.drop(columns='0')
+        # order columns
+        hosps = hosps[['<1y','1-4y','5-17y','18-39y','40-64y','>=65y']]
+    if incidence:
+        # divide by age group population, matched by year
+        hosps = hosps.div(age_by_year.loc[hosps.index.year].values)
+    return hosps
 
-monthly_flu_hospitalizations = flu_hospitalizations.groupby(["Year","Month"]).size().reset_index(name='Count')
-monthly_rsv_hospitalizations = rsv_hospitalizations.groupby(["Year","Month"]).size().reset_index(name='Count')
-monthly_covid_hospitalizations = covid_hospitalizations.groupby(["Year","Month"]).size().reset_index(name='Count')
+monthly_flu_hospitalizations = month_format(flu_hospitalizations,incidence=True)
+monthly_rsv_hospitalizations = month_format(rsv_hospitalizations,incidence=True)
+monthly_covid_hospitalizations = month_format(covid_hospitalizations,incidence=True)
 
-# fill in zeros for missing months
-for date in pd.date_range(start='2015-10-01',end='2023-09-30',freq='MS'):
-    year = date.year
-    month = date.month
-    if not ((monthly_flu_hospitalizations["Year"] == year) & (monthly_flu_hospitalizations["Month"] == month)).any():
-        monthly_flu_hospitalizations = pd.concat([monthly_flu_hospitalizations,pd.DataFrame({"Year":[year],"Month":[month],"Count":[0]})],ignore_index=True)
-    if not ((monthly_rsv_hospitalizations["Year"] == year) & (monthly_rsv_hospitalizations["Month"] == month)).any():
-        monthly_rsv_hospitalizations = pd.concat([monthly_rsv_hospitalizations,pd.DataFrame({"Year":[year],"Month":[month],"Count":[0]})],ignore_index=True)
-    if not ((monthly_covid_hospitalizations["Year"] == year) & (monthly_covid_hospitalizations["Month"] == month)).any():
-        monthly_covid_hospitalizations = pd.concat([monthly_covid_hospitalizations,pd.DataFrame({"Year":[year],"Month":[month],"Count":[0]})],ignore_index=True)
+fig, ax = plt.subplots(3,1,figsize=(6.5,8.5),sharex=True)
+colors = hsv(-0.02+np.arange(6)/6)
+# colors = ['#648FFF','#DC267F','#FFB000','#785EF0','#FF832B','#000000']
+# colors = ['red','orange','yellow','green','blue','purple']
+monthly_flu_hospitalizations.plot(ax=ax[0],color=colors,label=AGE_GROUP_NAMES[1:],title='Flu hospitalizations')
+monthly_rsv_hospitalizations.plot(ax=ax[1],color=colors,label=AGE_GROUP_NAMES[1:],title='RSV hospitalizations')
+monthly_covid_hospitalizations.plot(ax=ax[2],color=colors,label=AGE_GROUP_NAMES[1:],title='COVID-19 hospitalizations')
+ax[1].set_ylabel('Number of hospitalizations')
+ax[2].set_xlabel('Date')
+plt.tight_layout()
+plt.savefig('Figures/KPSC_hospitalizations_incidence_by_age.png')
 
-monthly_flu_hospitalizations["Date"] = pd.to_datetime(monthly_flu_hospitalizations["Year"].astype(str) + '-' + monthly_flu_hospitalizations["Month"].astype(str) + '-01')
-monthly_rsv_hospitalizations["Date"] = pd.to_datetime(monthly_rsv_hospitalizations["Year"].astype(str) + '-' + monthly_rsv_hospitalizations["Month"].astype(str) + '-01')
-monthly_covid_hospitalizations["Date"] = pd.to_datetime(monthly_covid_hospitalizations["Year"].astype(str) + '-' + monthly_covid_hospitalizations["Month"].astype(str) + '-01')
+# # # save to csv
+# monthly_flu_hospitalizations.to_csv('Data/Processed/KPSC_flu_hosp_incidence_by_age.csv',index=False)
+# monthly_rsv_hospitalizations.to_csv('Data/Processed/KPSC_rsv_hosp_incidence_by_age.csv',index=False)
+# monthly_covid_hospitalizations.to_csv('Data/Processed/KPSC_covid_hosp_incidence_by_age.csv',index=False)
 
-# sort by date
-monthly_flu_hospitalizations = monthly_flu_hospitalizations.sort_values(by="Date")
-monthly_rsv_hospitalizations = monthly_rsv_hospitalizations.sort_values(by="Date")
-monthly_covid_hospitalizations = monthly_covid_hospitalizations.sort_values(by="Date")
+# # flu_hospitalizations["Relative"] = flu_hospitalizations["Count"]/flu_hospitalizations["Count"].max()
+# # rsv_hospitalizations["Relative"] = rsv_hospitalizations["Count"]/rsv_hospitalizations["Count"].max()
+# # covid_hospitalizations["Relative"] = covid_hospitalizations["Count"]/covid_hospitalizations["Count"].max()
 
-# # save to csv
-monthly_flu_hospitalizations.to_csv('Data/Processed/KPSC_flu_hosp.csv',index=False)
-monthly_rsv_hospitalizations.to_csv('Data/Processed/KPSC_rsv_hosp.csv',index=False)
-monthly_covid_hospitalizations.to_csv('Data/Processed/KPSC_covid_hosp.csv',index=False)
-
-# monthly_flu_hospitalizations["Relative"] = monthly_flu_hospitalizations["Count"]/monthly_flu_hospitalizations["Count"].max()
-# monthly_rsv_hospitalizations["Relative"] = monthly_rsv_hospitalizations["Count"]/monthly_rsv_hospitalizations["Count"].max()
-# monthly_covid_hospitalizations["Relative"] = monthly_covid_hospitalizations["Count"]/monthly_covid_hospitalizations["Count"].max()
-
-# fig, ax = plt.subplots(2,1,figsize=(6.5,6.5),sharex=True)
-# ax[0].plot(monthly_flu_hospitalizations["Date"],monthly_flu_hospitalizations["Count"],color='#648FFF',label='Flu')
-# ax[0].plot(monthly_rsv_hospitalizations["Date"],monthly_rsv_hospitalizations["Count"],color='#DC267F',label='RSV')
-# ax[0].plot(monthly_covid_hospitalizations["Date"],monthly_covid_hospitalizations["Count"],color='#FFB000',label='COVID-19')
-# ax[0].set_title('Hospitalizations by month')
-# ax[0].set_ylabel('Number of hospitalizations')
-# ax[0].legend()
-# ax[1].plot(monthly_flu_hospitalizations["Date"],monthly_flu_hospitalizations["Relative"],color='#648FFF',label='Flu')
-# ax[1].plot(monthly_rsv_hospitalizations["Date"],monthly_rsv_hospitalizations["Relative"],color='#DC267F',label='RSV')
-# ax[1].plot(monthly_covid_hospitalizations["Date"],monthly_covid_hospitalizations["Relative"],color='#FFB000',label='COVID-19')
-# ax[1].set_xlabel('Date')
-# ax[1].set_ylabel('Proportion of maximum hospitalizations')
+# fig, ax = plt.subplots(1,1,figsize=(6.5,6.5),sharex=True)
+# # ax[0].plot(flu_hospitalizations["Date"],flu_hospitalizations["Count"],color='#648FFF',label='Flu')
+# ax.plot(rsv_hospitalizations["Date"],rsv_hospitalizations["Count"],color='#DC267F',label='RSV')
+# # ax[0].plot(covid_hospitalizations["Date"],covid_hospitalizations["Count"],color='#FFB000',label='COVID-19')
+# ax.set_title('Hospitalizations by month')
+# ax.set_ylabel('Number of hospitalizations')
+# ax.legend()
+# # ax[1].plot(flu_hospitalizations["Date"],flu_hospitalizations["Relative"],color='#648FFF',label='Flu')
+# # ax[1].plot(rsv_hospitalizations["Date"],rsv_hospitalizations["Relative"],color='#DC267F',label='RSV')
+# # ax[1].plot(covid_hospitalizations["Date"],covid_hospitalizations["Relative"],color='#FFB000',label='COVID-19')
+# ax.set_xlabel('Date')
+# # ax[1].set_ylabel('Proportion of maximum hospitalizations')
 # plt.tight_layout()
-# plt.savefig('Figures/KPSC_flu_COVID_and_RSV_hospitalizations_by_month_relative.png')
+# plt.savefig('Figures/KPSC_RSV_hospitalizations_by_month_relative.png')
