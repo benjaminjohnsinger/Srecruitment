04c479c857cf8a596efcbf2be0902ed933b679b1
BenSinger01 <bjsinger@ln002.brc>
2025-02-25 11:33:12 -0800

    edits to running DE on savio

--- Analyses/Cluster_Programmes/run_DE.sh
+++ Analyses/Cluster_Programmes/run_DE.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=defluA
+#SBATCH --job-name=deRSV
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --output=lhnmfluA_%j.out
-#SBATCH --error=lhnmfluA_%j.err
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=bjsinger@berkeley.edu
 ## Command(s) to run:
 
 module load python/3.11.6-gcc-11.4.0
-ipython Analyses/fit_opt.py InfluenzaA 250225
+ipython Analyses/fit_opt.py RSV 250225
