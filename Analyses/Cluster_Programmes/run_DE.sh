8af8b4ce15f6b3eb5582e9d9102d6518711f3bbd
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-03-05 15:36:32 -0800

    a copule stylistic things

--- Analyses/Cluster_Programmes/run_DE.sh
+++ Analyses/Cluster_Programmes/run_DE.sh
@@ -11,4 +11,4 @@
 ## Command(s) to run:
 
 module load python/3.11.6-gcc-11.4.0
-ipython Analyses/fit_opt.py InfluenzaA 250304 YoungEarly X X 15 1 0.7
+ipython Analyses/fit_opt.py InfluenzaA 250305 YoungEarly X X 15 1 0.7
