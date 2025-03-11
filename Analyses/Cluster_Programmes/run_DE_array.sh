0f9aa32679e98902d1ea106320606b1c5a3aec09
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-03-10 17:30:33 -0700

    second part of no import array)

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=rcnb
+#SBATCH --job-name=rcarray2
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --array=0-3
+#SBATCH --array=4-11
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -14,16 +14,18 @@
 module load python/3.11.6-gcc-11.4.0
 
 combinations=(
-"InfluenzaA 250310 Mobility nb nr 15 1 0.7"
-"InfluenzaA 250310 FlexStepwise nb nr 15 1 0.7"
-"InfluenzaB 250310 Mobility nb nr 15 1 0.7"
-"InfluenzaB 250310 FlexStepwise nb nr 15 1 0.7"
-"InfluenzaA 250310 Mobility nb X 15 1 0.7"
-"InfluenzaA 250310 FlexStepwise nb X 15 1 0.7"
-"InfluenzaB 250310 Mobility nb X 15 1 0.7"
-"InfluenzaB 250310 FlexStepwise nb X 15 1 0.7"
-"RSV 250310 Mobility nb X 15 1 0.7"
-"RSV 250310 FlexStepwise nb X 15 1 0.7"
+"InfluenzaA 250306 Stepwise ni nr 15 1 0.7"
+"InfluenzaA 250306 Mobility ni nr 15 1 0.7"
+"InfluenzaA 250306 YoungEarly ni nr 15 1 0.7"
+"InfluenzaA 250306 FlexStepwise ni nr 15 1 0.7"
+"InfluenzaB 250306 Stepwise ni nr 15 1 0.7"
+"InfluenzaB 250306 Mobility ni nr 15 1 0.7"
+"InfluenzaB 250306 YoungEarly ni nr 15 1 0.7"
+"InfluenzaB 250306 FlexStepwise ni nr 15 1 0.7"
+"RSV 250306 Stepwise ni X 15 1 0.7"
+"RSV 250306 Mobility ni X 15 1 0.7"
+"RSV 250306 YoungEarly ni X 15 1 0.7"
+"RSV 250306 FlexStepwise ni X 15 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
