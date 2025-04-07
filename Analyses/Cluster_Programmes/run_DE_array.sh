cc0b097d561760fa29210b6fd850396f9c7e0f80
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-04-07 10:17:49 -0700

    update date

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -14,18 +14,18 @@
 module load python/3.11.6-gcc-11.4.0
 
 combinations=(
-"RSV 250325 FlexStepwise X flexage 0.01 15 1 0.7"
-"RSV 250325 FlexStepwise setimport flexage 0.01 15 1 0.7"
-"InfluenzaA 250325 FlexStepwise X flexage 0.01 15 1 0.7"
-"InfluenzaA 250325 FlexStepwise setimport flexage 0.01 15 1 0.7"
-"InfluenzaB 250325 FlexStepwise X flexage 0.01 15 1 0.7"
-"InfluenzaB 250325 FlexStepwise setimport flexage 0.01 15 1 0.7"
-"Metapneumovirus 250325 FlexStepwise X flexage 0.01 15 1 0.7"
-"Metapneumovirus 250325 FlexStepwise setimport flexage 0.01 15 1 0.7"
-"Adenovirus 250325 FlexStepwise X flexage 0.01 15 1 0.7"
-"Adenovirus 250325 FlexStepwise setimport flexage 0.01 15 1 0.7"
-"Parainfluenza3 250325 FlexStepwise X flexage 0.01 15 1 0.7"
-"Parainfluenza3 250325 FlexStepwise setimport flexage 0.01 15 1 0.7"
+"RSV 250407 FlexStepwise X flexage 0.01 15 1 0.7"
+"RSV 250407 FlexStepwise setimport flexage 0.01 15 1 0.7"
+"InfluenzaA 250407 FlexStepwise X flexage 0.01 15 1 0.7"
+"InfluenzaA 250407 FlexStepwise setimport flexage 0.01 15 1 0.7"
+"InfluenzaB 250407 FlexStepwise X flexage 0.01 15 1 0.7"
+"InfluenzaB 250407 FlexStepwise setimport flexage 0.01 15 1 0.7"
+"Metapneumovirus 250407 FlexStepwise X flexage 0.01 15 1 0.7"
+"Metapneumovirus 250407 FlexStepwise setimport flexage 0.01 15 1 0.7"
+"Adenovirus 250407 FlexStepwise X flexage 0.01 15 1 0.7"
+"Adenovirus 250407 FlexStepwise setimport flexage 0.01 15 1 0.7"
+"Parainfluenza3 250407 FlexStepwise X flexage 0.01 15 1 0.7"
+"Parainfluenza3 250407 FlexStepwise setimport flexage 0.01 15 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
