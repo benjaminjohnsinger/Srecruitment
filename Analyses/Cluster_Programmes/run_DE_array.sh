143466de70570c02f41a1b26aecce60e72d51c6a
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-08-05 16:01:13 -0700

    trying really low tolerance to get variation within 1.92

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -14,10 +14,10 @@
 module load python/3.11.6-gcc-11.4.0
 
 combinations=(
-"InfluenzaA 250805701701 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
-"Metapneumovirus 250805701701 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
-"InfluenzaA 2508057017012 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
-"Metapneumovirus 2508057017012 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
+"InfluenzaA 2508057013 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
+"Metapneumovirus 2508057013 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
+"InfluenzaA 2508057014 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
+"Metapneumovirus 2508057014 FlexStepwise season_infections flexage 0.01 20 1 0.7 0.0001"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
