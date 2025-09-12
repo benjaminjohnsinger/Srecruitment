62302931d68481ba5e74d1cc5b584db47cdb403c
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-09-12 13:52:37 -0700

    hm error seems to have mysteriously fixed itself

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=fc_coronamodel
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
-#SBATCH --time=1:00:00
-#SBATCH --array=0-1
+#SBATCH --time=72:00:00
+#SBATCH --array=0-35
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -12,7 +12,7 @@
 ## Command(s) to run:
 
 module load python/3.11.6-gcc-11.4.0
-source ~/jax-cpu-env/bin/activate
+source ~/match-env/bin/activate
 
 combinations=(
 "RSV 250908 Taube NA flexage 1e-9 20 1 0.7"
--- Analyses/fit_opt.py
+++ Analyses/fit_opt.py
@@ -105,9 +105,8 @@ if __name__ == '__main__':
     multiprocessing.set_start_method('spawn', force=True)
     printstr = "neg_log_likelihood," + ",".join([key for key in bounds_dict.keys()])
     print(printstr)
-    opt = sp.optimize.differential_evolution(likelihood,bounds,popsize=desize,mutation=(0.5,max_mutation),recombination=recombination,init="halton",maxiter=1,seed=seed,
-    workers = 4)
-    # workers=int(os.getenv('SLURM_CPUS_ON_NODE')))
+    opt = sp.optimize.differential_evolution(likelihood,bounds,popsize=desize,mutation=(0.5,max_mutation),recombination=recombination,init="halton",seed=seed,
+    workers=int(os.getenv('SLURM_CPUS_ON_NODE')))
 
     with open("Data/Processed/DE_opt_"+pathogen+lockdown+option1+option2+str(seed)+".pickle","wb") as f:
         pickle.dump(opt,f)
