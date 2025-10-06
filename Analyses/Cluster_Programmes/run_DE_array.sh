b8359853a36dc82663c27f6b57ad1826af8a89dd
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-10-06 16:46:52 -0700

    limited test version of cluster code

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --array=0-23
+#SBATCH --array=0-3
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -16,29 +16,9 @@ source ~/match-env/bin/activate
 
 combinations=(
 "RSV 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"Adenovirus 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 250908 Mobility NA flexage 1e-9 20 1 0.7"
 "RSV 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"Adenovirus 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2509082 Mobility NA flexage 1e-9 20 1 0.7"
 "RSV 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Adenovirus 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
 "RSV 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Adenovirus 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
