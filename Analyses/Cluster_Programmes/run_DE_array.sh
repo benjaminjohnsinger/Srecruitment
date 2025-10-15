e56ecaa30a59e1a1c1ea1dd301c143790f575af7
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-10-14 17:51:10 -0700

    mobility and test

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --array=0-19
+#SBATCH --array=0-25
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -15,26 +15,32 @@ module load python/3.11.6-gcc-11.4.0
 source ~/match-env/bin/activate
 
 combinations=(
-"InfluenzaA 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Adenovirus 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"Adenovirus 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"Adenovirus 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Adenovirus 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 250908 Mobility NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 2509082 Mobility NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 250908 Mobility2 NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 2509082 Mobility2 NA flexage 1e-9 20 1 0.7"
+"RSV 251014 Mobility NA flexage 1e-9 20 1 0.7"
+"RSV 2510142 Mobility NA flexage 1e-9 20 1 0.7"
+"RSV 251014 Mobility2 NA flexage 1e-9 20 1 0.7"
+"RSV 2510142 Mobility2 NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 251014 Mobility NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 2510142 Mobility NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 251014 Mobility2 NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 2510142 Mobility2 NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 251014 Mobility NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 2510142 Mobility NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 251014 Mobility2 NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 2510142 Mobility2 NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 251014 Mobility NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 2510142 Mobility NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 251014 Mobility2 NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 2510142 Mobility2 NA flexage 1e-9 20 1 0.7"
+"Adenovirus 251014 Mobility NA flexage 1e-9 20 1 0.7"
+"Adenovirus 2510142 Mobility NA flexage 1e-9 20 1 0.7"
+"Adenovirus 251014 Mobility2 NA flexage 1e-9 20 1 0.7"
+"Adenovirus 2510142 Mobility2 NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 251014 Mobility NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 2510142 Mobility NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 251014 Mobility2 NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 2510142 Mobility2 NA flexage 1e-9 20 1 0.7"
+"test 251014 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"test 2510142 FlexStepwise NA flexage 1e-9 20 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
