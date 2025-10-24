10ce05a6a62b7fdc34b5eb176998f68c9dc37a09
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-10-24 15:08:53 -0700

    run DE on savio with extra waning and maternal immunity

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=newdataDE
+#SBATCH --job-name=mimmwane
 #SBATCH --account=fc_coronamodel
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
@@ -15,18 +15,18 @@ module load python/3.11.6-gcc-11.4.0
 source ~/match-env/bin/activate
 
 combinations=(
-"RSV 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"RSV 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"Metapneumovirus 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"Adenovirus 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"Adenovirus 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"Parainfluenza3 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 251024 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"RSV 2510242 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"Metapneumovirus 251024 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"Metapneumovirus 2510242 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"Adenovirus 251024 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"Adenovirus 2510242 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"Parainfluenza3 251024 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"Parainfluenza3 2510242 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"InfluenzaA 251024 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"InfluenzaA 2510242 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"InfluenzaB 251024 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
+"InfluenzaB 2510242 FlexStepwise maxmimmwane flexage 1e-9 20 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
