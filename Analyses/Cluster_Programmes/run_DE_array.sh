6e359c6a386f7cbcb6818721571994d3b51765ec
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-10-24 14:35:54 -0700

    testing new fitting on savio

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=testDE
+#SBATCH --job-name=newDEtst
 #SBATCH --account=fc_coronamodel
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --array=0-3
+#SBATCH --array=0-1
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -15,10 +15,8 @@ module load python/3.11.6-gcc-11.4.0
 source ~/match-env/bin/activate
 
 combinations=(
-"test 251015 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"test 2510152 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"test 2510153 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"test 2510154 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 251015 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 2510152 FlexStepwise NA flexage 1e-9 20 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
