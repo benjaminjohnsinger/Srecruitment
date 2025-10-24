0e2619fdc8d6667d25f5f7d6902b36f67af0b343
BenSinger01 <benjaminjohnsinger@gmail.com>
2025-10-24 14:48:23 -0700

    run DE on savio with new data

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=newDEtst
+#SBATCH --job-name=newdataDE
 #SBATCH --account=fc_coronamodel
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --array=0-1
+#SBATCH --array=0-11
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -15,8 +15,18 @@ module load python/3.11.6-gcc-11.4.0
 source ~/match-env/bin/activate
 
 combinations=(
-"RSV 251015 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"RSV 2510152 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Adenovirus 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Adenovirus 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 251024 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 2510242 FlexStepwise NA flexage 1e-9 20 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
