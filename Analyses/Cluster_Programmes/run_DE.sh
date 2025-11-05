fe626c3d611c3adbe5fbad2177d8e56248b7b09a
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2025-11-04 18:19:56 -0800

    contact model DE bug fix

--- Analyses/Cluster_Programmes/run_DE.sh
+++ Analyses/Cluster_Programmes/run_DE.sh
@@ -11,4 +11,4 @@
 ## Command(s) to run:
 
 module load python/3.11.6-gcc-11.4.0
-ipython Analyses/cm_opt.py RSV,Metapneumovirus,InfluenzaA,InfluenzaB,Adenovirus,Parainfluenza3 2511032,2511032,2511032,241104,2511032,2511032 maxmimmwane,maxmimmwane,maxmimmwane,wane,maxmimmwane,maxmimmwane flexage 20 1 0.7 >> Outputs/CM_Outputs/251104.csv
+ipython Analyses/cm_opt.py RSV,Metapneumovirus,InfluenzaA,InfluenzaB,Adenovirus,Parainfluenza3 2511032,2511032,2511032,2411042,2511032,2511032 maxmimmwane,maxmimmwane,maxmimmwane,wane,maxmimmwane,maxmimmwane flexage 20 1 0.7 >> Outputs/CM_Outputs/251104.csv
