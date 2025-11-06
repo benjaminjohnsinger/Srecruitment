d6fb92a11846c0e2ccfb51f15dcfdfc91a09ba19
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2025-11-06 13:10:13 -0800

    cm wane with no flu B

--- Analyses/Cluster_Programmes/run_DE.sh
+++ Analyses/Cluster_Programmes/run_DE.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=nofluBcm
+#SBATCH --job-name=nofluBcmwane
 #SBATCH --account=fc_coronamodel
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
@@ -11,4 +11,4 @@
 ## Command(s) to run:
 
 module load python/3.11.6-gcc-11.4.0
-ipython Analyses/cm_opt.py RSV,Metapneumovirus,InfluenzaA,Adenovirus,Parainfluenza3 2511032,2511032,2511032,2511032,2511032 NA,NA,NA,NA,NA flexage 20 1 0.7 >> Outputs/CM_Outputs/251104.csv
+ipython Analyses/cm_opt.py RSV,Metapneumovirus,InfluenzaA,Adenovirus,Parainfluenza3 251105,251105,251105,251105,251105 wane,wane,wane,wane,wane flexage 20 1 0.7 >> Outputs/CM_Outputs/251105.csv
