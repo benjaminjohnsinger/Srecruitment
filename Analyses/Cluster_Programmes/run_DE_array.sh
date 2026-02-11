46d00b523769bc85d023a81ec218ef1c6d6a6499
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-02-10 16:32:20 -0800

    new cluster programme

--- Analyses/Cluster_Programmes/run_DE_array.sh
+++ Analyses/Cluster_Programmes/run_DE_array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=newvax
+#SBATCH --job-name=ppnewvx
 #SBATCH --account=fc_coronamodel
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --array=0-8
+#SBATCH --array=0-17
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -15,15 +15,24 @@ module load python/3.11.6-gcc-11.4.0
 source ~/match-env/bin/activate
 
 combinations=(
-"RSV 260126 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"RSV 2601262 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"RSV 2601263 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 260126 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2601262 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaA 2601263 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 260126 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2601262 FlexStepwise NA flexage 1e-9 20 1 0.7"
-"InfluenzaB 2601263 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 260210 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 2602102 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"RSV 2602103 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 260210 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 2602102 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaA 2602103 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 260210 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 2602102 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"InfluenzaB 2602103 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 260210 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 2602102 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Metapneumovirus 2602103 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Adenovirus 260210 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Adenovirus 2602102 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Adenovirus 2602103 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 260210 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 2602102 FlexStepwise NA flexage 1e-9 20 1 0.7"
+"Parainfluenza3 2602103 FlexStepwise NA flexage 1e-9 20 1 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
