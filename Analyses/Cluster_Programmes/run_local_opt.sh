97b03220410f3b7257f4b591ce363feada285a51
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-02-25 09:16:58 -0800

    test how long optax takes on Savio

--- Analyses/Cluster_Programmes/run_local_opt.sh
+++ Analyses/Cluster_Programmes/run_local_opt.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=lhnmfluA
+#SBATCH --job-name=optax
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio2
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
-#SBATCH --output=lhnmfluA_%j.out
-#SBATCH --error=lhnmfluA_%j.err
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=bjsinger@berkeley.edu
 ## Command(s) to run:
 
 module load python/3.11.6-gcc-11.4.0
-ipython Analyses/local_opt.py RSV 24
+ipython Analyses/fit_opt.py Metapneumovirus 260224 FlexStepwise NA flexagep01 1e-9 1000 0.003 3
