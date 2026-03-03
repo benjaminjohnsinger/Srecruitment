0037df460e7f148bdc5a963c245aada4994bd92a
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-03-02 17:14:36 -0800

    fully jax and cuda compliant differential evolution with evosax

--- Analyses/Cluster_Programmes/run_gpu.sh
+++ Analyses/Cluster_Programmes/run_gpu.sh
@@ -26,4 +26,4 @@
 module purge
 source activate /global/scratch/users/bjsinger/jax_env
 
-python -u Analyses/fit_opt.py RSV 260302 FlexStepwise NA flexagep01 1e-9 400 0.7 400
+python -u Analyses/fit_opt.py RSV 260302 FlexStepwise NA flexagep01 1e-9 20 0.7 400
