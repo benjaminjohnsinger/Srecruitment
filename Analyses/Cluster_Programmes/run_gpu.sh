d7a9743b005c2a701316c52cf5c97b2979448e22
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-03-16 18:15:52 -0700

    try optax on gpu again

--- Analyses/Cluster_Programmes/run_gpu.sh
+++ Analyses/Cluster_Programmes/run_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=evosax
+#SBATCH --job-name=optax
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio4_gpu
 #SBATCH --nodes=1
@@ -21,16 +21,16 @@
 #SBATCH --mail-user=bjsinger@berkeley.edu
 
 # Wall clock limit:
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 
 module purge
 module load anaconda3
 source activate /global/scratch/users/bjsinger/jax_env
 
 
-python -u Analyses/fit_opt.py RSV 260316 Exponential NA flexagep01 1e-9 200 1000 0.7
-python -u Analyses/fit_opt.py Metapneumovirus 260316 Exponential NA flexagep01 1e-9 200 1000 0.7
-python -u Analyses/fit_opt.py Adenovirus 260316 Exponential NA flexagep01 1e-9 200 1000 0.7
-python -u Analyses/fit_opt.py Parainfluenza3 260316 Exponential NA flexagep01 1e-9 200 1000 0.7
-python -u Analyses/fit_opt.py InfluenzaA 260316 Exponential NA flexagep01 1e-9 200 1000 0.7
-python -u Analyses/fit_opt.py InfluenzaB 260316 Exponential NA flexagep01 1e-9 200 1000 0.7
+python -u Analyses/fit_opt.py RSV 260316 Exponential NA flexagep01 1e-9 100 2000 0.5 optax
+python -u Analyses/fit_opt.py Metapneumovirus 260316 Exponential NA flexagep01 1e-9 100 2000 0.5 optax
+python -u Analyses/fit_opt.py Adenovirus 260316 Exponential NA flexagep01 1e-9 100 2000 0.5 optax
+python -u Analyses/fit_opt.py Parainfluenza3 260316 Exponential NA flexagep01 1e-9 100 2000 0.5 optax
+python -u Analyses/fit_opt.py InfluenzaA 260316 Exponential NA flexagep01 1e-9 100 2000 0.5 optax
+python -u Analyses/fit_opt.py InfluenzaB 260316 Exponential NA flexagep01 1e-9 100 2000 0.5 optax
