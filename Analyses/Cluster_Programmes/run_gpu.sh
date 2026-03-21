0a7ab64d538bb96d755b1198b2abb31e50f1cf32
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-03-20 17:10:43 -0700

    try mimm for different pathogens

--- Analyses/Cluster_Programmes/run_gpu.sh
+++ Analyses/Cluster_Programmes/run_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=RSVseeds
+#SBATCH --job-name=mimm
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio4_gpu
 #SBATCH --nodes=1
@@ -12,7 +12,7 @@
 #SBATCH --time=10:00:00
 
 # Array job specifications:
-#SBATCH --array=0-5
+#SBATCH --array=0-4
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 
@@ -20,7 +20,7 @@ module purge
 module load anaconda3
 source activate /global/scratch/users/bjsinger/jax_env
 
-SEEDS=(260320 2603202 2603203 2603204 2603205 2603206)
-CURRENT_SEED=${SEEDS[$SLURM_ARRAY_TASK_ID]}
-echo "Starting optimization for: $CURRENT_SEED (Task ID: $SLURM_ARRAY_TASK_ID)"
-python -u Analyses/fit_opt.py RSV $CURRENT_SEED Exponential mimm flexagep01 1e-9 200 1000 0.7
+PATHOGENS=("Metapneumovirus" "Adenovirus" "Parainfluenza3" "InfluenzaA" "InfluenzaB")
+CURRENT_PATHOGEN=${PATHOGENS[$SLURM_ARRAY_TASK_ID]}
+echo "Starting optimization for: $CURRENT_PATHOGEN (Task ID: $SLURM_ARRAY_TASK_ID)"
+python -u Analyses/fit_opt.py $CURRENT_PATHOGEN 260320 Exponential mimm flexagep01 1e-9 200 1000 0.7
