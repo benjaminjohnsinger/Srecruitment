857815ac22acde24d6dd1bfd4952eb45c5069db9
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-04-16 18:30:29 -0700

    run long gpu fits with what is hopefully the final form of the model

--- Analyses/Cluster_Programmes/run_gpu.sh
+++ Analyses/Cluster_Programmes/run_gpu.sh
@@ -21,12 +21,12 @@ module load anaconda3
 source activate /global/scratch/users/bjsinger/jax_env
 
 combinations=(
-"RSV 260415 ExponentialInOut maxmimmsplit daycarep5maxagep028 1e-9 200 4000 0.7"
-"Metapneumovirus 260415 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
-"InfluenzaA 260415 ExponentialInOut maxmimmsplit daycarep5maxagep05 1e-9 200 4000 0.7"
-"InfluenzaB 260415 ExponentialInOut maxmimmsplit daycarep5maxagep05 1e-9 200 4000 0.7"
-"Adenovirus 260415 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
-"Parainfluenza3 260415 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
+"RSV 260416 ExponentialInOut maxmimmsplit daycarep5maxagep028 1e-9 200 4000 0.7"
+"Metapneumovirus 260416 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
+"InfluenzaA 260416 ExponentialInOut maxmimmsplit daycarep5maxagep05 1e-9 200 4000 0.7"
+"InfluenzaB 260416 ExponentialInOut maxmimmsplit daycarep5maxagep05 1e-9 200 4000 0.7"
+"Adenovirus 260416 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
+"Parainfluenza3 260416 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
