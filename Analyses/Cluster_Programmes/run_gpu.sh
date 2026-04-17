b02a78fdcf099f891534e78abd0bb2fe09f562f3
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-04-16 18:21:38 -0700

    run long gpu fits with what is hopefully the final form of the model

--- Analyses/Cluster_Programmes/run_gpu.sh
+++ Analyses/Cluster_Programmes/run_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=split
+#SBATCH --job-name=long
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio4_gpu
 #SBATCH --nodes=1
@@ -9,7 +9,7 @@
 #SBATCH --qos=a5k_gpu4_normal
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=bjsinger@berkeley.edu
-#SBATCH --time=10:00:00
+#SBATCH --time=72:00:00
 
 # Array job specifications:
 #SBATCH --array=0-8
@@ -21,15 +21,12 @@ module load anaconda3
 source activate /global/scratch/users/bjsinger/jax_env
 
 combinations=(
-"RSV 260415 Exponential split daycarep5maxagep028 1e-9 200 2000 0.7"
-"Metapneumovirus 260415 Exponential split daycarep5maxagep02 1e-9 200 2000 0.7"
-"InfluenzaA 260415 Exponential split daycarep5maxagep05 1e-9 200 2000 0.7"
-"InfluenzaB 260415 Exponential split daycarep5maxagep05 1e-9 200 2000 0.7"
-"Adenovirus 260415 Exponential split daycarep5maxagep02 1e-9 200 2000 0.7"
-"Parainfluenza3 260415 Exponential split daycarep5maxagep02 1e-9 200 2000 0.7"
-"RSV 260415 Exponential splitmaxmimm daycarep5maxagep028 1e-9 200 2000 0.7"
-"RSV 260415 ExponentialByAge7 split daycarep5maxagep028 1e-9 200 2000 0.7"
-"RSV 260415 ExponentialByAge5 splitmimm daycarep5maxagep028 1e-9 200 2000 0.7"
+"RSV 260415 ExponentialInOut maxmimmsplit daycarep5maxagep028 1e-9 200 4000 0.7"
+"Metapneumovirus 260415 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
+"InfluenzaA 260415 ExponentialInOut maxmimmsplit daycarep5maxagep05 1e-9 200 4000 0.7"
+"InfluenzaB 260415 ExponentialInOut maxmimmsplit daycarep5maxagep05 1e-9 200 4000 0.7"
+"Adenovirus 260415 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
+"Parainfluenza3 260415 ExponentialInOut maxmimmsplit daycarep5maxagep02 1e-9 200 4000 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
