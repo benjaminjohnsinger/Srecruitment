ee70303d33f8ee7c1b64ca9e05edb216e17fd3d2
Ben Singer <33283075+BenSinger01@users.noreply.github.com>
2026-06-13 17:02:57 -0700

    trying different ihr for hmpv odipp25, and trying odipp5

--- Analyses/Cluster_Programmes/run_gpu.sh
+++ Analyses/Cluster_Programmes/run_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=ODipp25
+#SBATCH --job-name=ODipp
 #SBATCH --account=ac_idmodels
 #SBATCH --partition=savio4_gpu
 #SBATCH --nodes=1
@@ -12,7 +12,7 @@
 #SBATCH --time=24:00:00
 
 # Array job specifications:
-#SBATCH --array=0-5
+#SBATCH --array=0-6
 #SBATCH --output=%x_%A_%a.out
 #SBATCH --error=%x_%A_%a.err
 
@@ -21,12 +21,14 @@ module load anaconda3
 source activate /global/scratch/users/bjsinger/jax_env
 
 combinations=(
-"Adenovirus 260612 ExponentialODipp25 dedupsac maxagep003 1e-9 200 2000 0.7"
-"InfluenzaA 260612 ExponentialODipp25 dedupsac maxagep035 1e-9 200 2000 0.7"
-"RSV 260612 ExponentialODipp25 dedupsac maxagep028 1e-9 200 2000 0.7"
-"Metapneumovirus 260612 ExponentialODipp25 dedupsac maxagep015 1e-9 200 2000 0.7"
-"Parainfluenza3 260612 ExponentialODipp25 dedupsac maxagep004 1e-9 200 2000 0.7"
-"InfluenzaB 260612 ExponentialODipp25 dedupsac maxagep035 1e-9 200 2000 0.7"
+"Metapneumovirus 260612 ExponentialODipp25 dedupsac maxagep01 1e-9 200 2000 0.7"
+
+"Adenovirus 260612 ExponentialODipp5 dedupsac maxagep003 1e-9 200 2000 0.7"
+"InfluenzaA 260612 ExponentialODipp5 dedupsac maxagep035 1e-9 200 2000 0.7"
+"RSV 260612 ExponentialODipp5 dedupsac maxagep028 1e-9 200 2000 0.7"
+"Metapneumovirus 260612 ExponentialODipp5 dedupsac maxagep015 1e-9 200 2000 0.7"
+"Parainfluenza3 260612 ExponentialODipp5 dedupsac maxagep004 1e-9 200 2000 0.7"
+"InfluenzaB 260612 ExponentialODipp5 dedupsac maxagep035 1e-9 200 2000 0.7"
 )
 
 combination="${combinations[$SLURM_ARRAY_TASK_ID]}"
